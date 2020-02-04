259c02dbd357076d234a1eb1fd357681ab924c0f
vazgen.danielyan <vazgen.danielyan@sflpro.com>
2020-02-05 01:26:47 +0400

    Small changes. Moved files into appropriate packages.

--- src/finonacci/Fibonacci.java
+++ src/finonacci/Fibonacci.java
@@ -1,3 +1,5 @@
+package finonacci;
+
 /* class fibonacci
     {
         static int fib(int n)
@@ -29,18 +31,24 @@ public class Fibonacci {
 */
 
 public class Fibonacci {
+
+    public static void main(String[] args) {
+        fib(4);
+    }
+
     static void fib(int n) {
-        int a = 0, b = 1;
+        int a = 0;
+        int b = 1;
+
         for (int i = 1; i <= n; ++i) {
-            System.out.print(a + " , ");
+            print(a);
             int sum = a + b;
             a = b;
             b = sum;
         }
     }
-    public static void main(String[] args){
-        fib(4);
+
+    private static void print(final int a) {
+        System.out.print(a + " , ");
     }
-
-
 }
--- src/testing/HelloWorld.java
+++ src/testing/HelloWorld.java
@@ -1,8 +1,9 @@
+package testing;
+
 public class HelloWorld {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 
         System.out.println("Hello World");
     }
-
-
 }
