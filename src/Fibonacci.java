7b340972fdc3f6ed478435fa8d4babc5f2445f4d
MarinaS <smarishkas23@gmail.com>
2020-02-05 00:30:55 +0400

    Simple method of printing Fibonacci Sequence

---
+++ src/Fibonacci.java
@@ -0,0 +1,46 @@
+/* class fibonacci
+    {
+        static int fib(int n)
+        {
+            if (n <= 1)
+                return n;
+            return fib(n-1) + fib(n-2);
+        }
+
+        public static void main (String args[])
+        {
+            int n = 9;
+            System.out.println(fib(n));
+        }
+    }
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10, a = 0, b = 1;
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(a + " , ");
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+    }
+}
+*/
+
+public class Fibonacci {
+    static void fib(int n) {
+        int a = 0, b = 1;
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(a + " , ");
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+    }
+    public static void main(String[] args){
+        fib(4);
+    }
+
+
+}
